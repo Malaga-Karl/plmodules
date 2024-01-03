f4a062598e7ad71605359518618f1f12e1ad5264
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-01-03 15:06:53 +0800

    mod 10

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,43 @@
+# Module 1
+
+# class Employee:
+#     id = 10
+#     name = 'John'
+#     def display(self):
+#         print(f'ID: {self.id}\nName:{self.name}')
+
+# class myClass:
+#     def method1(self):
+#         print('Pamantasan ng Lungsod ng Maynila')
+
+#     def method2(self,someString):
+#         print(f'Software Testing: {someString}')
+
+# def main():
+#     c = myClass()
+#     c.method2('HELLO')
+
+# if __name__ == '__main__':
+#     main()
+#--------------------------------
+
+# class Parrot:
+#     species = 'bird'
+#     def __init__(self, name, age, species):
+#         self.name = name
+#         self.age = age
+#         self.species = species
+
+# def main():
+#     blu = Parrot('Blu', 10, 'spix')
+
+#     print(isinstance(blu, Parrot))
+
+# if __name__ == '__main__':
+#     main()
+
+#####################
+
+# Module 2
+
+print(isinstance(4, int))
